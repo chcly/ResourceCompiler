c20063df6d4e9b4b0098d8a2b5ba98a90d682ee2
Charlie C <snailrose@gmail.com>
2023-02-11 13:21:28 -0700

    Update

--- Test/Test1.cpp
+++ Test/Test1.cpp
@@ -74,3 +74,23 @@ GTEST_TEST(ResourceCompiler, RC_002)
     ExpectContainsString(OutputFile("out_001.cpp"),
                          "// was unable to read any data from the supplied file, so");
 }
+
+GTEST_TEST(ResourceCompiler, RC_003)
+{
+    const String out = Console::launch(
+        TargetFile("ResourceCompiler"),
+        " -o ",
+        OutputFile("out_002"),
+        " ",
+        TestFile("gen_002.txt"));
+
+    ExpectContainsString(OutputFile("out_002.h"),
+                         "static void getGen_002(ByteArray &dest);");
+    ExpectContainsString(OutputFile("out_002.cpp"), "gen_002[108]");
+    ExpectContainsString(OutputFile("out_002.cpp"),
+                         "0x54,0x68,0x69,0x73,0x20,0x69,0x73,0x20,0x6d,0x65,0x61,0x6e,0x74,");
+    ExpectContainsString(OutputFile("out_002.cpp"),
+                         "0x69,0x6c,0x65,0x72,0x20,0x74,0x65,0x73,0x74,0x20,0x52,0x43,0x5f,");
+    ExpectContainsString(OutputFile("out_002.cpp"),
+                         "0x74,0x65,0x64,0x20,0x66,0x6f,0x72,0x20,0x20,0x0d,0x0a,0x65,0x71,");
+}
